877209ee0b82414810542c7b40dd53c4f8a82c31
joão Paulo <joaopaulomoreirarodrigues@gmail.com>
2022-03-29 00:20:41 -0300

    insertion sort

---
+++ estrutura_dados/ordenação/insertion_sort/insertion_sort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#define TAM 10
+
+using namespace std;
+void dump (int vetor[TAM]);
+
+void insertion_sort(int vetor[TAM]){
+    int atual;
+    int j;
+    int i;
+
+    for(i=1;i<TAM;i++){
+        //elemento atual em analise
+        atual = vetor[i];
+        //elemento anterior ao analiusado
+        j = i - 1;
+    }
+    //analisando membros anteriores
+    while((j >= 0) && (atual < vetor[j])){
+        //posiciona os elementos uma posição para frente
+        vetor[j+1] = vetor[j];
+        //faz o j andar para atras
+        j--;
+    }
+
+    //agora que temos espaço colocamos o atual na posicao correta
+    vetor[j+1] = atual;
+}
+
+int main () {
+    int vetor[TAM] = {10,9,8,7,6,5,4,3,2,1};
+    insertion_sort(vetor);
+    dump(vetor);
+
+}
+
+void dump (int vetor[TAM]){
+	for(int i=0;i<TAM;i++){
+		cout << "[" << i << "] => ";
+		cout<< vetor[i] << " ";
+	}
+}
