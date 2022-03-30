e3f337ed63182366ee850a525831fb9adec129c0
joão Paulo <joaopaulomoreirarodrigues@gmail.com>
2022-03-30 20:29:30 -0300

    lista e insertion short

--- estrutura_dados/ordenação/bubble_sort/README.md
+++ estrutura_dados/ordenação/bubble_sort/README.md
@@ -2,6 +2,10 @@
 
 -----
 
+![](https://upload.wikimedia.org/wikipedia/commons/0/06/Bubble-sort.gif)
+
+-----
+
 ![](https://lh5.googleusercontent.com/_oLwPF5ZvaZZ4pGD-HvSUSw6nTwwHjUwcLpNigUvb24-PKNwjMUwXcWYWf2wp4HopzHkh9JVmZd_AFYP4HjSYelidbw4FRo1fHrWV3KxbFM13xlRLALb-y-EbLhEmln11lhwEZPV)
 
 https://blog.betrybe.com/tecnologia/bubble-sort-tudo-sobre/
--- estrutura_dados/ordenação/insertion_sort/insertion_sort.cpp
+++ estrutura_dados/ordenação/insertion_sort/insertion_sort.cpp
@@ -7,26 +7,33 @@ using namespace std;
 void dump (int vetor[TAM]);
 
 void insertion_sort(int vetor[TAM]){
+
     int atual;
     int j;
     int i;
 
+    //o valor da posição 0 ja esta ordenado pq n há ngm meeor que ele a esquerda
+    //por isso começamos em 1
+
+
     for(i=1;i<TAM;i++){
-        //elemento atual em analise
+        //a cada loop do for a variavel atual armazena a posição I atual do vetor
         atual = vetor[i];
-        //elemento anterior ao analiusado
+        //elemento anterior sendo analisado
         j = i - 1;
-    }
-    //analisando membros anteriores
-    while((j >= 0) && (atual < vetor[j])){
-        //posiciona os elementos uma posição para frente
-        vetor[j+1] = vetor[j];
-        //faz o j andar para atras
-        j--;
+        //analisando membros anteriores
+        while((j >= 0) && (atual < vetor[j])){
+            //posiciona os elementos uma posição para frente
+            vetor[j+1] = vetor[j];
+            //faz o j andar para atras
+            j--;
+        }
+        //agora que temos espaço colocamos o atual na posicao correta
+        vetor[j+1] = atual;
     }
 
-    //agora que temos espaço colocamos o atual na posicao correta
-    vetor[j+1] = atual;
+
+
 }
 
 int main () {
